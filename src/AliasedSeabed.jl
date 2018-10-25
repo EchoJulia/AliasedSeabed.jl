c3d7db2495ce269c49960b93b70378ad34b0ad8f
Rob Blackwell <rob@robblackwell.com>
2018-10-25 14:12:45 +0100

    Support echograms with no ASB

--- src/AliasedSeabed.jl
+++ src/AliasedSeabed.jl
@@ -30,15 +30,17 @@ function blackwell_asbmask(Sv, ntheta, nphi; Ttheta=702, Tphi=282, dtheta=28, dp
     _m1 = m1(ntheta, Ttheta=Ttheta, dtheta=dtheta)
     _m2 = m2(nphi, Tphi=Tphi, dphi=dphi)
              
-    # m1 = meanfilter(ntheta, dtheta, dtheta).^2 .> Ttheta  
-    # m2  = meanfilter(nphi, dphi, dphi).^2  .> Tphi
-
     m = (_m1 .| _m2)
-
+    
     a = Sv[m]
 
     b = filter(x -> x !=-999, a) # remove 'missing' cells
 
+    if sum(b) < 1
+        # blank mask
+        return map(x -> false, Sv)
+    end
+    
     md = median(b)
 
     # info("md $md")
