15b32bf839be774592edbe99f851d64f6e67b319
Rob Blackwell <rob@robblackwell.com>
2019-04-25 13:05:17 +0100

    Clean up following paper on arXiv.org

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
 # AliasedSeabed
 
+![Lifecycle](https://img.shields.io/badge/lifecycle-experimental-orange.svg)
+
 [![Build Status](https://travis-ci.org/robblackwell/AliasedSeabed.jl.svg?branch=master)](https://travis-ci.org/robblackwell/AliasedSeabed.jl)
 
 [![Coverage Status](https://coveralls.io/repos/robblackwell/AliasedSeabed.jl/badge.svg?branch=master&service=github)](https://coveralls.io/github/robblackwell/AliasedSeabed.jl?branch=master)
 
 [![codecov.io](http://codecov.io/github/robblackwell/AliasedSeabed.jl/coverage.svg?branch=master)](http://codecov.io/github/robblackwell/AliasedSeabed.jl?branch=master)
+
+## Introduction
+
+A Julia implementation of the aliased seabed detection algorithm described in 
+[Blackwell et al. (2019)](https://arxiv.org/abs/1904.10736).
--- src/AliasedSeabed.jl
+++ src/AliasedSeabed.jl
@@ -3,8 +3,7 @@ module AliasedSeabed
 export blackwell_asbmask, m1, m2
 
 using Statistics
-using EchogramUtils
-using EchogramProcessing
+using ImageFiltering
 
 function m1(ntheta; Ttheta=702, dtheta=28)
     meanfilter(ntheta, dtheta, dtheta).^2 .> Ttheta
@@ -19,10 +18,69 @@ function myfindn(x)
     (getindex.(I, 1), getindex.(I, 2))
 end
 
+function meanfilter(A, height, width)
+    kernel  = Array{Float64}(undef, height, width)
+    kernel .= 1 / (width * height)
+    kernel = centered(kernel)
+    imfilter(A, kernel, Fill(0))
+end
+
+"""
+    bwselect(BW, c , r)
+Select objects in a binary image.
+Similar to the MATLAB function of the same name.
+"""
+function bwselect(BW, c, r)
+    # constants
+    north = CartesianIndex(-1,  0)
+    south = CartesianIndex( 1,  0)
+    east  = CartesianIndex( 0,  1)
+    west  = CartesianIndex( 0, -1)
+    
+    queue = CartesianIndex.(r,c)
+    
+    m,n = size(BW)
+    out = falses(m,n)
+    
+    while !isempty(queue)
+        node = pop!(queue)
+        
+        if BW[node]
+            wnode = node
+            enode = node + east
+        
+             # Move west until node is false
+            while checkbounds(Bool, BW, wnode) && BW[wnode] 
+                out[wnode] = true
+                if checkbounds(Bool, BW, wnode + north) && !out[wnode + north]
+                    push!(queue, wnode + north)
+                end
+                if checkbounds(Bool, BW, wnode + south) && !out[wnode + south]
+                    push!(queue, wnode + south)
+                end
+                wnode += west
+            end
+        
+            # Move east until node is false
+            while checkbounds(Bool, BW, enode) && BW[enode]
+                out[enode] = true
+                if checkbounds(Bool, BW, enode + north) && !out[enode + north]
+                    push!(queue, enode + north)
+                end
+                if checkbounds(Bool, BW, enode + south) && !out[enode + south]
+                    push!(queue, enode + south)
+                end
+                enode += east
+            end
+        end
+        
+    end
+    return out
+end
+
 """
 
-Implements aliased seabed detection according to the author's
-forthcoming paper.
+Aliased seabed detection according to [Blackwell et al. (2019)](https://arxiv.org/abs/1904.10736)
 
 """
 function blackwell_asbmask(Sv, ntheta, nphi; Ttheta=702, Tphi=282, dtheta=28, dphi=52, minSv=nothing)
@@ -61,8 +119,5 @@ function blackwell_asbmask(Sv, ntheta, nphi; Ttheta=702, Tphi=282, dtheta=28, dp
     bw .| m
 
 end
-
-
-
 
 end # module
